c1b0bf6f515a01b234696747f0a1974c907a5dcb
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-24 11:50:09 +0300

    a

--- password_entropy_calculator.js
+++ password_entropy_calculator.js
@@ -2,110 +2,34 @@ const inputField = document.getElementById("input_field");
 const outputField = document.getElementById("result_field");
 const button = document.getElementById("calculate_button");
 
-const startUpperCaseCode = 65;
-const endUpperCaseCode = 90;
-
-const startLowerCaseCode = 97;
-const endLowerCaseCode = 122;
-
-const startNumberCode = 48;
-const endNumberCode = 57;
-
-const startSpecialSymbolCode = 33;
-const endSpecialSymbolCode = 63;
-
-const totalChars = (endLowerCaseCode - startLowerCaseCode) + 1;
-const totalNumbers = (endNumberCode - startNumberCode) + 1;
-const totalSpecialSymbols = (endSpecialSymbolCode - startSpecialSymbolCode) + 1;
-
 function calculateEntropy(password){
 
     let H = 0.0;
 
     for(let i = 0;i < password.length;i++){
-        let probability = (!isDigit(password.charAt(i)) ? (isSpecialSymbol(password.charAt(i)) ? 1 / totalSpecialSymbols : 1 / totalChars) : 1/totalNumbers);
+        let probability = getProbability(password[i]);
         let log2 = Math.log2(probability);
 
         H += Math.abs((probability * log2));
     }
-
+    
     return Math.round(Math.abs(H));
 
 };
 
-function dataValidation(data){
+function getProbability(char){
+    if(isDigit(char))
+        return 1 / totalNumbers;
+    if(isSpecialSymbol(char))
+        return 1 / totalSpecialSymbols;
 
-    if(data == null || data == undefined)
-        return false;
-
-    for(let i = 0; i < data.length;i++){
-
-        let char = data.charAt(i);
-
-        if(!(isLowerCase(char) || isUpperCase(char) || isDigit(char) || isSpecialSymbol(char)))
-            return false;
-
-    }
-
-    return true;
+    return 1 / totalChars;
 }
 
-function isLowerCase(symbol){
-
-    if(symbol == null || symbol == undefined)
-        return false;
-
-    if(symbol.length != 1)
-        return false;
-
-    let code = symbol.charCodeAt(0);
-
-    return (code >= startLowerCaseCode && code <= endLowerCaseCode);
-}
-
-function isUpperCase(symbol){
-
-    if(symbol == null || symbol == undefined)
-        return false;
-
-    if(symbol.length != 1)
-        return false;
-
-    let code = symbol.charCodeAt(0);
-
-    return (code >= startUpperCaseCode && code <= endUpperCaseCode);
-
-}
-
-function isDigit(symbol){
-
-    if(symbol == null || symbol == undefined)
-        return false;
-
-    if(symbol.length != 1)
-        return false;
-
-    let code = symbol.charCodeAt(0);
-
-    return (code >= startNumberCode && code <= endNumberCode);
-}
-
-function isSpecialSymbol(symbol){
-
-    if(symbol == null || symbol == undefined)
-        return false;
-
-    if(symbol.length != 1)
-        return false;
-
-    let code = symbol.charCodeAt(0);
-
-    return (code >= startSpecialSymbolCode && code <= endSpecialSymbolCode);
-}
 
 button.addEventListener("click", ()=>{
     
-    if(!dataValidation(inputField.value)){
+    if(!validateString(inputField.value)){
         alert("Неверно заполнено поле пароля");
         outputField.value = 0.0;
         return;
