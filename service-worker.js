bfd4d9bd758d622cca9ada03ad2323dfdf53f85a
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-24 14:52:42 +0300

    activate

--- service-worker.js
+++ service-worker.js
@@ -16,6 +16,19 @@ self.addEventListener('install', event => {
   })());
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      ))
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith((async () => {
     const cache = await caches.open(CACHE_NAME);
