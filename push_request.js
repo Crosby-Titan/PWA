602eb352b39c928b27cbd48e932acbf0dfa7e6f5
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-24 15:18:26 +0300

    hng

--- push_request.js
+++ push_request.js
@@ -7,6 +7,15 @@ document.getElementById("notification_subscribe").addEventListener("click",async
 
     if (permission === "granted") {
         console.log("Получено разрешение на пуш уведомления.");
+
+        const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+
+        const subscribe = await serviceWorkerRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: window.Uint8Array.from("BN36xnlGjkL0FvjPDkCPCFPdIPzMBgfKnYG9q1k7XVOKoaxnt1D4WgsaZOYa7nGf_GP8CJ3tBHRTUjWKY5bvHPU")
+        });
+
+        alert(subscribe.toJSON());
     }
 });
 
