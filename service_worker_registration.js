09f51c93d738521b787d6e05e47120ac8d67d1e5
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-25 15:40:58 +0300

    sw

--- service_worker_registration.js
+++ service_worker_registration.js
@@ -2,10 +2,10 @@ navigator?.serviceWorker.getRegistrations().then(registrations => {
   let isServiceWorkerRegistered = false;
   let isFirebaseWorkerRegistered = false;
   registrations.forEach(registration => {
-      if (registration.active && registration.active.scriptURL === '/service-worker.js') {
+      if (registration.active && registration.active.scriptURL === 'https://crosby-titan.github.io/service-worker.js') {
           isServiceWorkerRegistered = true;
       }
-      if(registration.active && registration.active.scriptURL === '/firebase-messaging-sw.js'){
+      if(registration.active && registration.active.scriptURL === 'https://crosby-titan.github.io/firebase-messaging-sw.js'){
         isFirebaseWorkerRegistered = true;
       }
   });
