6fdad58b0e1826c1a83845bd384f05e421cc0a0f
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-25 12:49:58 +0300

    сы

--- service_worker_registration.js
+++ service_worker_registration.js
@@ -1,7 +1,15 @@
 navigator?.serviceWorker.register('/service-worker.js', { scope: '/' })
 .then(() => {
-  console.log('Service worker registered');
+  console.log('Service worker зарегистрирован.');
 })
 .catch((error) => {
-  console.error('Service worker registration failed:', error);
+  console.error('Ошибка регистрации Service worker:', error);
+});
+
+navigator?.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/' })
+.then(() => {
+    console.log('Service Worker для Firebase Messaging зарегистрирован');
+})
+.catch((error) => {
+    console.error('Ошибка регистрации Service Worker для Firebase Messaging:', error);
 });
