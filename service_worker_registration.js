7010dc49dc23564c493db1466bae843a5934a1a0
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-25 15:38:48 +0300

    cc

--- service_worker_registration.js
+++ service_worker_registration.js
@@ -1,15 +1,33 @@
-navigator?.serviceWorker.register('/service-worker.js', { scope: '/' })
-.then(() => {
-  console.log('Service worker зарегистрирован.');
-})
-.catch((error) => {
-  console.error('Ошибка регистрации Service worker:', error);
-});
-
-navigator?.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/' })
-.then(() => {
-    console.log('Service Worker для Firebase Messaging зарегистрирован');
-})
-.catch((error) => {
-    console.error('Ошибка регистрации Service Worker для Firebase Messaging:', error);
+navigator?.serviceWorker.getRegistrations().then(registrations => {
+  let isServiceWorkerRegistered = false;
+  let isFirebaseWorkerRegistered = false;
+  registrations.forEach(registration => {
+      if (registration.active && registration.active.scriptURL === '/service-worker.js') {
+          isServiceWorkerRegistered = true;
+      }
+      if(registration.active && registration.active.scriptURL === '/firebase-messaging-sw.js'){
+        isFirebaseWorkerRegistered = true;
+      }
+  });
+  if (!isServiceWorkerRegistered) {
+      navigator?.serviceWorker.register('/service-worker.js', { scope: '/' })
+      .then(() => {
+        console.log('Service worker зарегистрирован.');
+      })
+      .catch((error) => {
+        console.error('Ошибка регистрации Service worker:', error);
+      });
+  }
+  if(!isFirebaseWorkerRegistered){
+    
+    navigator?.serviceWorker.register('/firebase-messaging-sw.js', { scope: '/' })
+    .then(() => {
+        console.log('Service Worker для Firebase Messaging зарегистрирован');
+    })
+    .catch((error) => {
+        console.error('Ошибка регистрации Service Worker для Firebase Messaging:', error);
+    });
+  }
+}).catch(error => {
+  console.error('Error checking service worker registrations:', error);
 });
