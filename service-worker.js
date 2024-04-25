056f1e08b482b6d6bee08456d583dcba75d8f88b
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-25 12:42:44 +0300

    firebase-messaging.sw

--- service-worker.js
+++ service-worker.js
@@ -49,18 +49,3 @@ self.addEventListener('fetch', event => {
     }
   })());
 });
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Получено уведомление:', payload);
-  
-  const notification = JSON.parse(payload.notification);
-
-  const notificationTitle = notification.title;
-  const notificationOptions = {
-    body: notification.body,
-    icon: notification.icon,
-    click_action: notification.click_action
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
