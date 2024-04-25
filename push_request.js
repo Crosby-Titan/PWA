99e4160323922fbf4d04c60b7cce8c2b147cbd06
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-04-25 13:41:29 +0300

    per

--- push_request.js
+++ push_request.js
@@ -2,10 +2,10 @@ const button = document.getElementById("notification_subscribe");
 
 button.addEventListener("click",async () => {
 
-    navigator?.serviceWorker.controller.postMessage({ message: "notification_request"});
+    if (Notification.permission === 'default'){
+        const permission = await Notification.requestPermission();
 
-    navigator?.serviceWorker.controller.addEventListener("message",(event)=>{
-        const {message , title} = event.data;
-    });
+        console.log(`Permission: ${permission}`);
+    }
 });
 
