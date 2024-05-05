935483b20cb97d882be65abc7b35cfda0f6a4a56
Crosby-Titan <mazafaka2018huy@gmail.com>
2024-05-05 13:06:14 +0300

    remove messaging

--- notification.index.js
+++ notification.index.js
@@ -1,19 +1,5 @@
 const sendNotificationBtn = document.getElementById("send_notification_btn");
 
-const config = {
-    apiKey: "AIzaSyBJD0pFT23NP7l0itfzoys1vegruVxGag8",
-    authDomain: "telegrambot-b7130.firebaseapp.com",
-    databaseURL: "https://telegrambot-b7130-default-rtdb.firebaseio.com",
-    projectId: "telegrambot-b7130",
-    storageBucket: "telegrambot-b7130.appspot.com",
-    messagingSenderId: "528987238699",
-    appId: "1:528987238699:web:9f5771129cabc23945156e",
-    measurementId: "G-TJRXL9L0G3"
-};
-
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
-
 sendNotificationBtn.addEventListener("click", async (event)=>{
     const tokens = await getAllTokens();
 
